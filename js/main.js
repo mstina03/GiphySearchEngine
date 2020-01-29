493b2da58a7ce79211b7fac78e9b30aec48468b0
Tina Pollard <smithtina1203@aol.com>
2020-01-29 00:32:48 -0600

    add click function

--- js/main.js
+++ js/main.js
@@ -25,6 +25,27 @@ $(function () {
 });
 
 $(function () {
-    var keyword = $('#keyword').val();
-    alert(" Keyword: " + keyword);
+
+    $('#search').click(function (event) {
+        
+        var keyword = $('#keyword').val();
+        var count = $('#gifnumber').val();
+        var api_key = "ApvlAJSFKecV5nbL7Jdu7BIdSFUj2CPR";
+
+        var myUrl = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key="+ api_key + "&limit=" + count;
+        alert("URL = " + myUrl);
+
+        $.ajax({
+            url: myUrl,
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                console.log('succes: ' + data);
+                alert('succes: ' + data);
+            }
+            
+        }); 
+    });
+    
 });
