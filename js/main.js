7267961b7599055c48dee088bde85ebd5f22563d
Tina Pollard <smithtina1203@aol.com>
2020-02-03 22:53:27 -0600

    Add Bootstrap files

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 //Show pop-up on hover
-$(function () {
-    $("#linkedin").tooltip({
-        show: {
-            effect: "slideDown",
-            delay: 300
-        }
-    });
-});
-$(function () {
-    $("#gitHub").tooltip({
-        show: {
-            effect: "slideDown",
-            delay: 300
-        }
-    });
-});
-$(function () {
-    $("#emailMe").tooltip({
-        show: {
-            effect: "slideDown",
-            delay: 300
-        }
-    });
-});
+// $(function () {
+//     $("#linkedin").tooltip({
+//         show: {
+//             effect: "slideDown",
+//             delay: 300
+//         }
+//     });
+// });
+// $(function () {
+//     $("#gitHub").tooltip({
+//         show: {
+//             effect: "slideDown",
+//             delay: 300
+//         }
+//     });
+// });
+// $(function () {
+//     $("#emailMe").tooltip({
+//         show: {
+//             effect: "slideDown",
+//             delay: 300
+//         }
+//     });
+// });
 
 $(function () {
 
