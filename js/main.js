a87a14d60de9c32bf222a9aa798446143a872199
Tina Pollard <53983106+mstina03@users.noreply.github.com>
2020-02-06 13:47:23 -0600

    edit scss and parse and display json results

--- js/main.js
+++ js/main.js
@@ -4,30 +4,26 @@ $(function () {
     $('#search').click(function (event) {
 
         event.preventDefault();
-
+        $('.grid-container').empty();
+      
         var keyword = $('#keyword').val();
         var count = $('#gifnumber').val();
         var api_key = "ApvlAJSFKecV5nbL7Jdu7BIdSFUj2CPR";
 
-        var myUrl = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=" + api_key + "&limit=" + count;
+        var myUrl = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${api_key}&limit=${count}`;
 
-
-        $.get(myUrl).done( function( response ) {
-            console.log( response );
-        });
-/*
-        $.ajax({
-            url: myUrl,
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                console.log('succes: ' + data);
-                alert('succes: ' + data);
+        $.get(myUrl).done(function (response) {
+            console.log(response);
+            
+            for(var i =0; i < response.data.length; i++)
+            {
+                var imageURL = '"' + response.data[i].images.preview_webp.url + '"';
+                $('.grid-container').append(`<html><img id="grid-img" src=${imageURL}</html>`);
             }
-
+           
         });
- */       
+        
+
     });
 
 });
