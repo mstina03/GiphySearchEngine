0c3219c40ce9d60b9d370e4a37f4362d8c3ef6eb
Tina Pollard <smithtina1203@aol.com>
2020-02-06 23:34:51 -0600

    format document and edit home page layout

--- js/main.js
+++ js/main.js
@@ -5,38 +5,35 @@ $(function () {
 
         event.preventDefault();
         $('.grid-container').empty();
-             
+
         var keyword = $('#keyword').val();
         var count = $('#gifnumber').val();
-        
-        if(keyword == null || keyword =="")
-        {
+
+        if (keyword == null || keyword == "") {
             alert("Enter a keyword to continue..");
         }
-        if(count == null || count == ""){
+        if (count == null || count == "") {
             count = 1;
         }
-        if(count > 60)
-        {
+        if (count > 60) {
             alert("You entered a number greater than 60, only 60 images will be diplayed...")
             count = 60;
         }
-      
+
         var api_key = "ApvlAJSFKecV5nbL7Jdu7BIdSFUj2CPR";
 
         var myUrl = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${api_key}&limit=${count}`;
 
         $.get(myUrl).done(function (response) {
             console.log(response);
-            
-            for(var i =0; i < response.data.length; i++)
-            {
+
+            for (var i = 0; i < response.data.length; i++) {
                 var imageURL = '"' + response.data[i].images.preview_webp.url + '"';
                 $('.grid-container').append(`<html><img id="grid-img" src=${imageURL}</html>`);
             }
-           
+
         });
-        
+
 
     });
 
