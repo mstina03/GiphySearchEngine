76334ae3afb25753e4d57a6f62f27cf310ea3d1c
Tina Pollard <53983106+mstina03@users.noreply.github.com>
2020-02-07 08:34:54 -0600

    update validation and text alignment

--- js/main.js
+++ js/main.js
@@ -12,16 +12,19 @@ $(function () {
         if (keyword == null || keyword == "") {
             alert("Enter a keyword to continue..");
         }
-        if (count == null || count == "") {
-            count = 1;
+        if (keyword.length > 0) {
+            if (count == null || count == "" || count < 1) {
+                count = 1;
+                alert("You entered a count less than 1, the default count of 1 will be used...")
+            }
         }
+
         if (count > 60) {
             alert("You entered a number greater than 60, only 60 images will be diplayed...")
             count = 60;
         }
 
         var api_key = "ApvlAJSFKecV5nbL7Jdu7BIdSFUj2CPR";
-
         var myUrl = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${api_key}&limit=${count}`;
 
         $.get(myUrl).done(function (response) {
