20a1186bac17788e81afdabf82b563e830825c1d
Tina Pollard <53983106+mstina03@users.noreply.github.com>
2020-02-09 12:58:55 -0600

    Project review edits

--- js/main.js
+++ js/main.js
@@ -16,23 +16,21 @@ $(function () {
             if (count == null || count == "" || count < 1) {
                 count = 1;
                 alert("You entered a count less than 1, the default count of 1 will be used...")
-            }
-        }
-
-        if (count > 30) {
-            alert("You entered a number greater than 30, only 30 images will be diplayed...")
-            count = 30;
+            } else if (count > 30) {
+                alert("You entered a number greater than 30, only 30 images will be diplayed...")
+                count = 30;
+            }            
         }
 
         var api_key = "ApvlAJSFKecV5nbL7Jdu7BIdSFUj2CPR";
         var myUrl = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${api_key}&limit=${count}`;
 
         $.get(myUrl).done(function (response) {
-            console.log(response);
+            console.log(response);   // delete this from the final version!!!
 
             for (var i = 0; i < response.data.length; i++) {
                 var imageURL = '"' + response.data[i].images.preview_webp.url + '"';
-                $('.grid-container').append(`<html><img id="grid-img" src=${imageURL}</html>`);
+                $('.grid-container').append(`<img id="grid-img" src=${imageURL}>`);  // <===== accessibility??????
             }
 
         });
